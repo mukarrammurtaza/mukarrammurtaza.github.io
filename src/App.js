30fca26e966eaf3118a6343ef96d326aca57ed98
SAAD AMIR <saadamir318@gmail.com>
2024-01-27 16:41:47 +0500

    add background glow

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import BackgroundBlurSvg from "./components/HomeComponents/BackgroundBlurSvg";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
+      <BackgroundBlurSvg/>
       <Routes>
         <Route index element={<Home />}/>
       </Routes>
--- src/components/HomeComponents/HeroSection.js
+++ src/components/HomeComponents/HeroSection.js
@@ -3,10 +3,10 @@ const HeroSection = () => {
     <div className='section heroSection'>
         <aside className='heroAside'>
             <p>We are here to make world a better place for you</p>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" fill="none" stroke="var(--green)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><line x1="7" y1="7" x2="17" y2="17" ></line><polyline points="17 7 17 17 7 17"></polyline></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26" fill="none" stroke="var(--green)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><line x1="7" y1="7" x2="17" y2="17" ></line><polyline points="17 7 17 17 7 17"></polyline></svg>
         </aside>
         <div className='heroMain'>
-            <h1>You Bring Problems.<br/>We Provide Solutions.</h1>
+            <h1>You Bring <span className='heroMainHead'>Problems.</span> <br/>We Provide <span className='heroMainHead'>Solutions.</span> </h1>
         </div>
     </div>
   )
